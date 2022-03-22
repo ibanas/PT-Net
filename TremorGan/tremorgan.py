b4b44af72a75f44917c3952b63df4af0a53c4600
ibanas <ib.anas@hotmail.com>
2022-03-21 23:49:41 -0400

    Update tremorgan.py

--- TremorGan/tremorgan.py
+++ TremorGan/tremorgan.py
@@ -2,7 +2,7 @@ import numpy as np
 from Tensorflow.keras.models import load_model
 import pickle
 
-def get_model(h5_model_path, pickled_scaler_path):
+def get_model_scaler(h5_model_path, pickled_scaler_path):
 	model = load_model(h5_model)
 	with open(ickled_scaler_path, 'rb') as sc:
 		scaler = pickle.load(sc)
